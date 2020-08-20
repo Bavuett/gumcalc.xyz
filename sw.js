6b3a9df1ccfbd89321a94d0636c7f1125225aa15
Lorenzo Barretta <43916566+devlbd@users.noreply.github.com>
2020-08-20 19:41:18 +0200

    Initial Commit
    Adding some basic files and folders, first release coming soon! :)

---
+++ sw.js
@@ -0,0 +1,54 @@
+self.addEventListener("install", function(e){
+    e.waitUntil(
+        caches.open("gumroadCalculator-v0-1").then(function(cache){
+            return cache.addAll([
+		        "/Gumroad-Calculator/",
+                "/Gumroad-Calculator/index",
+		        "/Gumroad-Calculator/help",
+                "/Gumroad-Calculator/css/style.css",
+                "/Gumroad-Calculator/js/script.js",
+                "/Gumroad-Calculator/manifest.json",
+		        "/Gumroad-Calculator/resources/icon.ico",
+                "/Gumroad-Calculator/resources/512.png",
+                "/Gumroad-Calculator/resources/192.png",
+                "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event){
+    console.log("Browser fetched this URL: " + event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", function(event){
+    var cacheWhiteList = ["gumroadCalculator-v0-1"];
+    
+    event.waitUntil(
+        caches.keys().then(function(cacheNames){
+            return Promise.all(
+                cacheNames.map(function(cacheName){
+                    console.log("Deteleting Cache.");
+                    if (cacheWhiteList.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+if (navigator.share) {
+    navigator.share({
+      title: "Gumroad Calculator.",
+      text: "To be changed.",
+      url: "https://devlbd.github.io/Gumroad-Calculator",
+    })
+      .then(() => console.log('Successful share'))
+      .catch((error) => console.log('Error sharing', error));
+  }
