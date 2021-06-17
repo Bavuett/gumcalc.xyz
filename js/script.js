3bf9dfb3f2f0f7cf119c41f9973eb7bb8837e713
Lorenzo Barretta <43916566+devlbd@users.noreply.github.com>
2021-06-17 14:18:32 +0200

    Almost completed UI

--- js/script.js
+++ js/script.js
@@ -1,46 +1,7 @@
-// When the app starts, the account type is set to 0 (Classic Account) and
-// gets the buttons ready.
-sessionStorage.setItem("accountType", "0");
-
-$(document).ready(function(){
-   $("#classicAccount").addClass("active");
-});
-
-// When the user clicks the buttons, change the account type and changes
-// the buttons' style to make the user know what kind of account they did choose.
-$("#classicAccount").click(function(){
-    sessionStorage.setItem("accountType", "0");
-    $("#classicAccount").addClass("active");
-    $("#creatorAccount").removeClass("active");
- });
-
-$("#creatorAccount").click(function(){
-    sessionStorage.setItem("accountType", "1");
-    $("#classicAccount").removeClass("active");
-    $("#creatorAccount").addClass("active");
- });
-
-// Making the Donation Button look decent with a script.
-$(".buttonDonate").hover(function(){
-   $(".aButton").addClass("hover");
-}, function(){
-   $(".aButton").removeClass("hover");
-});
-
-// When the Checkbox is checkes, the script checks the state.
-// If it is checked, sets the value to 1. If it is not, sets the value to 0.
-$(".checkbox__input").click(function(){
-   if($(".checkbox__input").is(":checked")) {
-      sessionStorage.setItem("isGumroadDiscover", "1");
-   } else {
-      sessionStorage.setItem("isGumroadDiscover", "0");
-   }
-});
-
-$("#calculate").click(function(){
-   $(".amountBox").removeClass("notshowing");
-});
-
-$("#close").click(function(){
-   $(".amountBox").addClass("notshowing");
+window.addEventListener("load", function(){
+   $(".glass").addClass("closed");
+   this.setTimeout(function(){
+      $(".animation-loading").addClass("stopped");
+      console.log("Animation Paused");
+   }, 1500);
 });
