1537a81779720f87a57275b8b917385ee1eba984
Lorenzo Barretta <43916566+devlbd@users.noreply.github.com>
2021-06-19 19:09:54 +0200

    Adding Custom PWA Install Dialog. Fixes to CSS.
    This update adds a custom PWA Installation Process, and fixes some UI errors.

--- js/script.js
+++ js/script.js
@@ -88,12 +88,21 @@ window.addEventListener("scroll", function(){
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
-  // Stash the event so it can be triggered later.
   deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
   $(".install").addClass("active");
-  // Optionally, send analytics event that PWA install promo was shown.
-  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
+$(".install").click(function() {
+   // Show the install prompt
+   deferredPrompt.prompt();
+   // Wait for the user to respond to the prompt
+   const { outcome } = await deferredPrompt.userChoice;
+   console.log(`User response to the install prompt: ${outcome}`);
+   deferredPrompt = null;
+   $(".install").removeClass("active");
+});
+
+window.addEventListener('appinstalled', () => {
+   $(".install").removeClass("active");
 });
