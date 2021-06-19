a3eed523ac73ea12690fc6d75df400a1e77ae9bb
Lorenzo Barretta <43916566+devlbd@users.noreply.github.com>
2021-06-19 13:55:33 +0200

    Testing PWA Support.

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ self.addEventListener("install", function(e){
 		        "/Gumroad-Calculator/resources/icon.ico",
                 "/Gumroad-Calculator/resources/512.png",
                 "/Gumroad-Calculator/resources/192.png",
-                "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+                "/Gumroad-Calculator/resources/jquery.js"
             ]);
         })
     );
