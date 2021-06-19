529ef9428ced66f94114c0b7623d11a716e71dbf
Lorenzo Barretta <43916566+devlbd@users.noreply.github.com>
2021-06-19 14:12:44 +0200

    Removing unnecessary pages.

--- sw.js
+++ sw.js
@@ -1,16 +1,13 @@
 self.addEventListener("install", function(e){
     e.waitUntil(
-        caches.open("gumroadCalculator-v0-1").then(function(cache){
+        caches.open("gumroad-calculator-v0-1").then(function(cache){
             return cache.addAll([
 		        "/Gumroad-Calculator/",
-                "/Gumroad-Calculator/index",
-		        "/Gumroad-Calculator/help",
                 "/Gumroad-Calculator/css/style.css",
                 "/Gumroad-Calculator/js/script.js",
                 "/Gumroad-Calculator/manifest.json",
-		        "/Gumroad-Calculator/resources/icon.ico",
                 "/Gumroad-Calculator/resources/512.png",
-                "/Gumroad-Calculator/resources/192.png",
+                "/Gumroad-Calculator/resources/152.png",
                 "/Gumroad-Calculator/resources/jquery.js"
             ]);
         })
@@ -27,7 +24,7 @@ self.addEventListener("fetch", function(event){
 });
 
 self.addEventListener("activate", function(event){
-    var cacheWhiteList = ["gumroadCalculator-v0-1"];
+    var cacheWhiteList = ["gumroad-calculator-v0-1"];
     
     event.waitUntil(
         caches.keys().then(function(cacheNames){
